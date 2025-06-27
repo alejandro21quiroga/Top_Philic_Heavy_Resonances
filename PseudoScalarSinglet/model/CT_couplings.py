768a42e026ddf58fb3f777d0278d73af89a8c23b
Alejandro Quiroga Trivino <alejandro.quiroga@correounivalle.edu.co>
2025-06-27 15:57:52 +0200

    top-philic

---
+++ PseudoScalarSinglet/model/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.4.58
+# Mathematica version: 12.1.1 for Mac OS X x86 (64-bit) (June 19, 2020)
+# Date: Fri 26 Feb 2021 09:58:55
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ PseudoScalarSinglet/model/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.4.58
+# Mathematica version: 12.1.1 for Mac OS X x86 (64-bit) (June 19, 2020)
+# Date: Fri 26 Feb 2021 09:58:55
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
