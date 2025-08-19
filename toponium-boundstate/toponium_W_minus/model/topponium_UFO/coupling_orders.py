1c80f2e9af279e27b20b7bf6977c98ea5e6be986
Alejandro Quiroga Trivino <alejandro.quiroga@correounivalle.edu.co>
2025-08-19 14:43:18 +0200

    top-philic

---
+++ toponium-boundstate/toponium_W_minus/model/topponium_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.4.68
+# Mathematica version: 10.4.1 for Mac OS X x86 (64-bit) (April 11, 2016)
+# Date: Thu 23 Apr 2020 12:52:17
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NEW = CouplingOrder(name = 'NEW',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
---
+++ toponium-boundstate/toponium_W_plus/model/topponium_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.4.68
+# Mathematica version: 10.4.1 for Mac OS X x86 (64-bit) (April 11, 2016)
+# Date: Thu 23 Apr 2020 12:52:17
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
